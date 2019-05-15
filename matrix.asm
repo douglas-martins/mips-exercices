9e4556fec87cea29ef05cf582e811baf5a47a600
Douglas Martins <douglasfabiamartins@hotmail.com>
2019-05-15 04:07:05 -0300

    some testing with methods

--- matrix.asm
+++ matrix.asm
@@ -10,48 +10,38 @@
 
 
 	.data
-Matrix_A 	          .word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 # Declaração da Matrix A (max matrix_A[4][4])
-Matrix_B 	          .word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 # Declaração da Matrix B (max matrix_B[4][4])
-Matrix_C 	          .word 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 # Declaração da Matrix C (max matrix_C[4][4])
+Matrix_A: 	          .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 # Declaração da Matrix A (max matrix_A[4][4])
+Matrix_B: 	          .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 # Declaração da Matrix B (max matrix_B[4][4])
+Matrix_C: 	          .word 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 # Declaração da Matrix C (max matrix_C[4][4])
 MsgEnterSizeColumn:       .asciiz "\nEntre com o tamanho da coluna (máx. = 4): "
 MsgEnterSizeRow:       	  .asciiz "\nEntre com o tamanho da linha (máx. = 4): "
-MsgWrongValue:      	  .asciiz "\nValor inválido."
-MsgInputValuesA:    	  .asciiz "\nMatrix_A["
-MsgInputValuesB:          .asciiz "\nMatrix_B["
-MsgInputValuesC:          .asciiz "\nMatrix_C["
-MsgInputValuesFinal:      .asciiz "] = " 
+MsgInputValuesBegin:   	  .asciiz "\nMatrix_"
+MsgInputValuesMiddle:     .asciiz "["
+MsgInputValuesFinal:      .asciiz "] = "
+MsgWrongValue:      	  .asciiz "\nValor inv�lido" 
 
 	.text
-main:
-	# MAIN VARIABLES
-	la   $s0, Matrix_A		# Carrega o Matrix_A[] no registrador $s0
-	la   $s1, Matrix_B		# Carrega o Matrix_B[] no registrador $s1
-	la   $s2, Matrix_A		# Carrega o Matrix_C[] no registrador $s2
-	addi $s3, $zero, 0		# Coluna_A = 0 no registrador $s3
-	addi $s4, $zero, 0		# Linha_A = 0 no registrador $s4
-	addi $s5, $zero, 0		# Coluna_B = 0 no registrador $s3
-	addi $s6, $zero, 0		# Linha_B = 0 no registrador $s4
-	addi $s7, $zero, 0		# i = 0 no registrador $s5
+j	main
 	
 Loop_Matrix_Size:
 Msg_Size:	
 	# PRINT READ SIZE MSG
 	li   $v0, 4			# Chamada 4 (syscall code para imprimit uma String)
-	la   $a0, MsgEnterSize		# MsgEnterSize
+	la   $a0, MsgEnterSizeColumn		# MsgEnterSize
 	syscall				# Executar a chamada do syscall
 	
 	# READ INT (SIZE)
 	li   $v0, 5			# Chamada 5 (syscall code para ler um int)
 	syscall				# Executar a chamada do syscall
-	add  $s1, $v0, $0		# Salva o valor condito em $v0, em $s1 (Numero lido)
+	add  $t1, $v0, $0		# Salva o valor condito em $v0, em $t1 (Numero lido)
 	
-	# CONDITION (to stay or exit the Loop_Array_Size)
-	slti $t0, $s1, 1		# if (size < 1) $t0 = 1 else $t0 = 0
-	bgt  $s1, 4 Incorrect_Size	# if (size > 4) goto Incorrect_Size
-	beq  $t0, $zero, Loop_Read	# if ($t0 == 0) goto Loop_Read
+	# CONDITION (to stay or exit the Loop_Matrix_Size)
+	slti $t0, $t1, 1		# if (size < 1) $t0 = 1 else $t0 = 0
+	bgt  $t1, 4 Incorrect_Size	# if (size > 4) goto Incorrect_Size
+	#beq  $t0, $zero, Loop_Read	# if ($t0 == 0) goto Loop_Read
 	beq  $t0, 1, Incorrect_Size	# if ($t0 == 1) goto Incorrect_Size
 	
-j	Loop_Matrix_Size			# goto Loop_Array_Size
+jr	$ra			# goto Loop_Array_Size
 
 Incorrect_Size:
 	# PRINT WRONG SIZE NUMBER MSG
@@ -59,43 +49,34 @@ Incorrect_Size:
 	la  $a0, MsgWrongValue		# MsgWrongValue
 	syscall				# Executar a chamada do syscall
 j	Msg_Size			# goto Msg_Size
+#---------- MSG_SIZE ----------#
 
-Loop_Read:
-	# CONDITION 
-	slt  $t0, $s2, $s1              # if (i < size) $t0 = 1 else $t0 = 0
-        beq  $t0, $zero, Prepare_Array  # if ($t0 == 0) goto Prepare_Array
-        
-        # DESLOCAMENTO
-        add  $t1, $s2, $s2         	# $t1 = 2.i  
-      	add  $t1, $t1, $t1         	# $t1 = 4.i
-      	add  $t1, $t1, $s0         	# $t1 = end.base + 4.i (deslocamento) = end. de A[i]
-      	
-      	# PRINT MsgInputValues1
-      	li   $v0, 4                	# chamada 4
-      	la   $a0, MsgInputValues1       # MsgInputValues1
-      	syscall
-      	
-      	# PRINT I
-      	li   $v0, 1                	# chamada 1
-      	add  $a0, $0, $s2 	  	# $s2 = index of array (i)
-      	syscall
-      	
-      	# PRINT MsgInputValues2
-      	li   $v0, 4                	# chamada 4
-      	la   $a0, MsgInputValues2       # MsgInputValues2
-      	syscall  
-      	
-      	# READ INT
-      	li   $v0, 5                	# chamada 5 
-      	syscall                    
-      	add  $s3, $v0, $0          	# salva $v0 em $s3
+main:
+	jal Loop_Matrix_Size
 	
-	# STORE $s4 IN A[i]
-      	sw   $s3, 0($t1)            	# salva $s4 no array A[i] 
-     
-     	# ADDING INDEX LOOP ++
-        addi $s2, $s2, 1          	# i++
+	add  $s3, $t1, 0 		# Coluna_A = 0 no registrador $s3
+	
+	jal Loop_Matrix_Size
+	
+	addi $s4, $t1, 0		# Linha_A = 0 no registrador $s4
+	
+	jal Loop_Matrix_Size
+	
+	addi $s5, $t1, 0		# Coluna_B = 0 no registrador $s3
+	
+	jal Loop_Matrix_Size
+	
+	addi $s6, $t1, 0		# Linha_B = 0 no registrador $s4
+	
+	# MAIN VARIABLES
+	la   $s0, Matrix_A		# Carrega o Matrix_A[] no registrador $s0
+	la   $s1, Matrix_B		# Carrega o Matrix_B[] no registrador $s1
+	la   $s2, Matrix_C		# Carrega o Matrix_C[] no registrador $s2
 
 
-j	Loop_Read			# goto Loop_Read	
+	#addi $s7, $zero, 0		# i = 0 no registrador $s5
+	
+	nop
+
+
 	
